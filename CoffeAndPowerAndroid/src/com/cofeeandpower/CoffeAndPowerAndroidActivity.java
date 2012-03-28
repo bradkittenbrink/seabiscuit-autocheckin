0dac77761e5af18ea31746697629a52731167c59
nikoladj <nikoladj@00070ff4-6079-4a5e-9430-ba2f2f32ff5d>
2012-03-28 16:02:34 +0000

    git-svn-id: https://svn.worklist.net/svn/repos/seabiscuit@6 00070ff4-6079-4a5e-9430-ba2f2f32ff5d

---
+++ CoffeAndPowerAndroid/src/com/cofeeandpower/CoffeAndPowerAndroidActivity.java
@@ -0,0 +1,13 @@
+package com.cofeeandpower;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoffeAndPowerAndroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
