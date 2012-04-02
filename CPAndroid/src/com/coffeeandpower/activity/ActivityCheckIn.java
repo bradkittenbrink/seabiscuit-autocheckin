39da6ddf56c789c0448959360a9ff1c8075fecd7
nikoladj <nikoladj@00070ff4-6079-4a5e-9430-ba2f2f32ff5d>
2012-04-02 10:58:00 +0000

    git-svn-id: https://svn.worklist.net/svn/repos/seabiscuit@17 00070ff4-6079-4a5e-9430-ba2f2f32ff5d

--- CPAndroid/src/com/coffeeandpower/activity/ActivityCheckIn.java
+++ CPAndroid/src/com/coffeeandpower/activity/ActivityCheckIn.java
@@ -1,16 +1,31 @@
 package com.coffeeandpower.activity;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 
 import com.coffeeandpower.R;
+import com.coffeeandpower.activity.ActivityMap.GeoUpdateHandler;
 import com.coffeeandpower.cont.Venue;
+import com.coffeeandpower.maps.MyOverlays;
 import com.coffeeandpower.views.CustomFontView;
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 
 public class ActivityCheckIn extends MapActivity{
 
 	private CustomFontView textTitle;
+	
+	// Map items
+	private MapView mapView;
+	private MapController mapController;
+	private MyOverlays itemizedoverlay;
+	private MyLocationOverlay myLocationOverlay;
 	
 	private Venue venue;
 	
@@ -34,15 +49,32 @@ public class ActivityCheckIn extends MapActivity{
 		
 		// Views
 		textTitle = (CustomFontView) findViewById(R.id.text_title);
+		mapView = (MapView) findViewById(R.id.imageview_mapview);
+		myLocationOverlay = new MyLocationOverlay(this, mapView);
+		Drawable drawable = this.getResources().getDrawable(R.drawable.loc_point);
+		itemizedoverlay = new MyOverlays(this, drawable);
+		
 		
 		// Views states
 		textTitle.setText(venue.getName());
+		
+		
+		// Set others
+		mapView.getOverlays().add(myLocationOverlay);
+		mapView.setClickable(false);
+		mapView.setEnabled(false);
+		mapController = mapView.getController();
+		mapController.setZoom(18);
+		
+		
+		// Navigate map to location from intent data
+		GeoPoint point = new GeoPoint((int)(venue.getLat()*1E6), (int)(venue.getLng()*1E6));
+		mapController.animateTo(point);
 	}
 
 	
 	@Override
 	protected void onResume() {
-		// TODO Auto-generated method stub
 		super.onResume();
 	}
 	
