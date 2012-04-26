e7afbaf46dd7456889f8a5e849185133c8269bb7
nikoladj <nikoladj@00070ff4-6079-4a5e-9430-ba2f2f32ff5d>
2012-04-26 12:22:05 +0000

    xmls and drawables 26.04.2012.
    git-svn-id: https://svn.worklist.net/svn/repos/seabiscuit@104 00070ff4-6079-4a5e-9430-ba2f2f32ff5d

---
+++ CPAndroid/src/com/coffeeandpower/activity/ActivityEnterInviteCode.java
@@ -0,0 +1,20 @@
+package com.coffeeandpower.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.coffeeandpower.R;
+
+public class ActivityEnterInviteCode extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_enter_invite_code);
+	}
+	
+	public void onClickInviteLater (View v){
+		
+	}
+
+}
