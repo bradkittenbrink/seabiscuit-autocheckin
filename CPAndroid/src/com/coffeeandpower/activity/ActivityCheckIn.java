6ea7e1f0e1fbf68864ac382b4819cd813be65caa
andres <andres@00070ff4-6079-4a5e-9430-ba2f2f32ff5d>
2012-06-22 18:51:24 +0000

    #17608 - Keyboard pushes checkin location view of screen
    git-svn-id: https://svn.worklist.net/svn/repos/seabiscuit@270 00070ff4-6079-4a5e-9430-ba2f2f32ff5d

--- CPAndroid/src/com/coffeeandpower/activity/ActivityCheckIn.java
+++ CPAndroid/src/com/coffeeandpower/activity/ActivityCheckIn.java
@@ -14,6 +14,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
